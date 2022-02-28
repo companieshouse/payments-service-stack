208c8f3aa870fb7e456c49048006d56db10ce01a
Edwin Mead <emead@companieshouse.gov.uk>
2022-02-28 15:15:27 +0000

    Redirect http to https

--- groups/stack/module-ecs-stack/load-balancer.tf
+++ groups/stack/module-ecs-stack/load-balancer.tf
@@ -21,6 +21,22 @@ resource "aws_lb_listener" "admin-web-lb-listener" {
   }
 }
 
+resource "aws_lb_listener" "admin-web-lb-listener" {
+  load_balancer_arn = aws_lb.front_end.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
 resource "aws_route53_record" "admin-web-r53-record" {
   count   = var.zone_id == "" ? 0 : 1 # zone_id defaults to empty string giving count = 0 i.e. not route 53 record
   zone_id = var.zone_id
